b4e7112fbaaf61875ff694425e4a19cb9447bd7c
stephenmayowa112 <stephenmayowa112@gmail.com>
2026-08-17 08:20:11 +0100

    Design: forced THAMES banner text overlay back to absolute vertical center

--- lib/data.ts
+++ lib/data.ts
@@ -77,6 +77,7 @@ export const projects = [
     heroImage: '/images/thames-banner-image.png',
     posterImage: '/images/thames-poster.png',
     logoImage: '/images/THAMES 1@2x.png',
+    logoClassName: '!top-1/2',
     details: {
       'Running Time': '95 minutes',
       Status: 'In development',
